12eef1e14ded05ef008dd175312606a6ae8abc7c
SimonKuchcinski <119090664+SimonKuchcinski@users.noreply.github.com>
2023-01-06 22:25:43 -0700

    Add files via upload

---
+++ 5_Number_Sum_Avg.py
@@ -0,0 +1,50 @@
+#Header
+print("This program will find the sum, and avg of five numbers input by the user.")
+print('')
+
+#obtains the five numbers from user
+num1= float(input("Your First Number is "))
+num2= float(input("Your Second Number is "))
+num3= float(input("Your Third Number is "))
+num4= float(input("Your Fourth Number is "))
+num5= float(input("Your Fifth Number is "))
+
+#adds up the five input values
+total= (num1 + num2 + num3 + num4 + num5)
+
+#finds the average of the five values 
+average= (total / 5)
+
+#spaces
+print()
+print()
+
+#shows the five values
+print("You Input the Values:", num1, num2, num3, num4, num5)
+
+#shows the total of the five values
+print("The Sum of the Values:",total)
+
+#shows the average of the five values
+print("The Average of the Values:", average)
+print('')
+
+print("You're all done, thank you!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
